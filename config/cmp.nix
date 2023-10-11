9abf0cb2d48e240543c071d97dc58909a357f3cf
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2023-10-11 02:15:30 +0200

    fixed lua snippet issue

--- config/cmp.nix
+++ config/cmp.nix
@@ -1,6 +1,7 @@
 ## Source: https://github.com/hmajid2301/dotfiles/blob/ab7098387426f73c461950c7c0a4f8fb4c843a2c/home-manager/editors/nvim/plugins/coding/cmp.nix
 {
     plugins = {
+      luasnip.enable = true;
       cmp-buffer = {
         enable = true;
       };
