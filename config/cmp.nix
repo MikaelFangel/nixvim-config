0eb783750219d28bda95aa32571be94d5d490080
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-03-19 21:41:33 +0100

    cmp: fixed suggestions

--- config/cmp.nix
+++ config/cmp.nix
@@ -16,6 +16,18 @@
       enable = true;
 
       settings = {
+        snippet.expand = "luasnip";
+        sources = [
+          { name = "nvim_lsp"; }
+          { name = "luasnip"; }
+          {
+            name = "buffer";
+            option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+          }
+          { name = "nvim_lua"; }
+          { name = "path"; }
+        ];
+        
         formatting = {
           fields = [ "abbr" "kind" "menu" ];
           format =
@@ -99,32 +111,32 @@
           "<C-e>" = "cmp.mapping.close()";
           "<CR>" =
             "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })";
-          "<Tab>" = 
-              # lua
-              ''
-                function(fallback)
-                  if cmp.visible() then
-                    cmp.select_next_item()
-                  elseif require("luasnip").expand_or_jumpable() then
-                    vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-                  else
-                    fallback()
-                  end
+          "<Tab>" =
+            # lua
+            ''
+              function(fallback)
+                if cmp.visible() then
+                  cmp.select_next_item()
+                elseif require("luasnip").expand_or_jumpable() then
+                  vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+                else
+                  fallback()
                 end
-              '';
-          "<S-Tab>" = 
-              # lua
-              ''
-                function(fallback)
-                  if cmp.visible() then
-                    cmp.select_prev_item()
-                  elseif require("luasnip").jumpable(-1) then
-                    vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-                  else
-                    fallback()
-                  end
+              end
+            '';
+          "<S-Tab>" =
+            # lua
+            ''
+              function(fallback)
+                if cmp.visible() then
+                  cmp.select_prev_item()
+                elseif require("luasnip").jumpable(-1) then
+                  vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+                else
+                  fallback()
                 end
-              '';
+              end
+            '';
         };
       };
     };
