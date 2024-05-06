d8a6fb7cc5de6f81675816d1177fd841efe970f4
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-05-06 10:25:14 +0200

    lsp-lines: enabled lsp lines

--- config/lsp.nix
+++ config/lsp.nix
@@ -1,23 +1,29 @@
 {
-  plugins.lsp = {
-    enable = true;
-    servers = {
-      bashls.enable = true;
-      clangd.enable = true;
-      elixirls.enable = true;
-      fsautocomplete.enable = true;
-      gopls.enable = true;
-      kotlin-language-server.enable = true;
-      nixd.enable = true;
-      ruff-lsp.enable = true;
+  plugins = {
+    lsp = {
+      enable = true;
+      servers = {
+        bashls.enable = true;
+        clangd.enable = true;
+        elixirls.enable = true;
+        fsautocomplete.enable = true;
+        gopls.enable = true;
+        kotlin-language-server.enable = true;
+        nixd.enable = true;
+        ruff-lsp.enable = true;
+      };
+      keymaps.lspBuf = {
+        "gd" = "definition";
+        "gD" = "references";
+        "gt" = "type_definition";
+        "gi" = "implementation";
+        "K" = "hover";
+      };
     };
-    keymaps.lspBuf = {
-      "gd" = "definition";
-      "gD" = "references";
-      "gt" = "type_definition";
-      "gi" = "implementation";
-      "K" = "hover";
+    lsp-lines = {
+      enable = true;
+      currentLine = true;
     };
+    rust-tools.enable = true;
   };
-  plugins.rust-tools.enable = true;
 }
