36880dcfdcea3d878563f2d7049d7023e27a7094
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-05-06 08:48:30 +0200

    keymaps: add keymaps for gitsigns and tabs (#59)
    * keymaps: added keymaps for git
    
    * keymaps: added keymaps for tabs
    
    * keymaps: added descriptions for prefixes
    
    * keymaps: added descritpions

--- config/default.nix
+++ config/default.nix
@@ -26,47 +26,106 @@
   globals.mapleader = " ";
 
   keymaps = [
-    # Global Mappings
+    # Global
     # Default mode is "" which means normal-visual-op
     {
-      # Toggle NvimTree
       key = "<C-n>";
       action = "<CMD>NvimTreeToggle<CR>";
+      options.desc = "Toggle NvimTree";
+    }
+
+    # File
+    {
+      mode = "n";
+      key = "<leader>f";
+      action = "+find/file";
     }
     {
       # Format file
       key = "<leader>fm";
       action = "<CMD>lua vim.lsp.buf.format()<CR>";
+      options.desc = "Format the current buffer";
     }
 
-    # Terminal Mappings
+    # Git    
+    {
+      mode = "n";
+      key = "<leader>g";
+      action = "+git";
+    }
+    {
+      mode = "n";
+      key = "<leader>gt";
+      action = "+toggles";
+    }
+    {
+      key = "<leader>gtb";
+      action = "<CMD>Gitsigns toggle_current_line_blame<CR>";
+      options.desc = "Gitsigns current line blame";
+    }
+    {
+      key = "<leader>gtd";
+      action = "<CMD>Gitsigns toggle_deleted";
+      options.desc = "Gitsigns deleted";
+    }
+    {
+      key = "<leader>gd";
+      action = "<CMD>Gitsigns diffthis<CR>";
+      options.desc = "Gitsigns diff this buffer";
+    }
+
+    # Tabs
+    {
+      mode = "n";
+      key = "<leader>t";
+      action = "+tab";
+    }
+    {
+      mode = "n";
+      key = "<leader>tn";
+      action = "<CMD>tabnew<CR>";
+      options.desc = "Create new tab";
+    }
+    {
+      mode = "n";
+      key = "<leader>td";
+      action = "<CMD>tabclose<CR>";
+      options.desc = "Close tab";
+    }
+    {
+      mode = "n";
+      key = "<leader>ts";
+      action = "<CMD>tabnext<CR>";
+      options.desc = "Go to the sub-sequent tab";
+    }
+    {
+      mode = "n";
+      key = "<leader>tp";
+      action = "<CMD>tabprevious<CR>";
+      options.desc = "Go to the previous tab";
+    }
+
+    # Terminal
     {
       # Escape terminal mode using ESC
       mode = "t";
       key = "<esc>";
       action = "<C-\\><C-n>";
+      options.desc = "Escape terminal mode";
     }
 
     # Rust
+    {
+      mode = "n";
+      key = "<leader>r";
+      action = "+rust";
+    }
     {
       # Start standalone rust-analyzer (fixes issues when opening files from nvim tree)
       mode = "n";
       key = "<leader>rs";
       action = "<CMD>RustStartStandaloneServerForBuffer<CR>";
+      options.desc = "Start standalone rust-analyzer";
     }
-
-    # {
-    #   # Mode can be a string or a list of strings
-    #   mode = "n";
-    #   key = "<leader>p";
-    #   action = "require('my-plugin').do_stuff";
-    #   lua = true;
-    #   # Note that all of the mapping options are now under the `options` attrs
-    #   options = {
-    #     silent = true;
-    #     desc = "My plugin does stuff";
-    #   };
-    # }
   ];
-
 }
