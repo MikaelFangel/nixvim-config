fb927328278a702a8e536ab52f75f0173922c416
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-05-28 10:51:02 +0200

    lsp: enable gleam lsp

--- config/lsp/default.nix
+++ config/lsp/default.nix
@@ -6,6 +6,7 @@
         bashls.enable = true;
         clangd.enable = true;
         elixirls.enable = true;
+        gleam.enable = true;
         gopls.enable = true;
         kotlin-language-server.enable = true;
         nixd.enable = true;
