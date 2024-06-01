297890bba528e954ab08629d8aa88d511533324f
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-06-01 22:08:52 +0200

    default: add copilot keymaps

--- config/default.nix
+++ config/default.nix
@@ -46,6 +46,36 @@
       action = "<CMD>TSContextToggle<CR>";
       options.desc = "Toggle Treesitter context";
     }
+    {
+      key = "<leader>ct";
+      action = "<CMD>CopilotChatToggle<CR>";
+      options.desc = "Toggle Copilot Chat Window";
+    }
+    {
+      key = "<leader>cs";
+      action = "<CMD>CopilotChatStop<CR>";
+      options.desc = "Stop current Copilot output";
+    }
+    {
+      key = "<leader>cr";
+      action = "<CMD>CopilotChatReview<CR>";
+      options.desc = "Review the selected code";
+    }
+    {
+      key = "<leader>ce";
+      action = "<CMD>CopilotChatExplain<CR>";
+      options.desc = "Give an explanation for the selected code";
+    }
+    {
+      key = "<leader>cd";
+      action = "<CMD>CopilotChatDocs<CR>";
+      options.desc = "Add documentation for the selection";
+    }
+    {
+      key = "<leader>cp";
+      action = "<CMD>CopilotChatTests<CR>";
+      options.desc = "Add tests for my code";
+    }
 
     # File
     {
