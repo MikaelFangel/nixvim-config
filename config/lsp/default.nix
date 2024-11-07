4615e6d76a45a4fbe9e86a1612edbf277a057094
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-11-08 00:29:42 +0100

    chore: rename swiProlog to swi-prolog

--- config/lsp/default.nix
+++ config/lsp/default.nix
@@ -14,7 +14,7 @@
         nixd.enable = true;
         prolog_ls = {
           enable = true;
-          package = pkgs.swiProlog;
+          package = pkgs.swi-prolog;
         };
         ruff_lsp.enable = true;
       };
